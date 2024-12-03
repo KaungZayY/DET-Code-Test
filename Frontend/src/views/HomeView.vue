5e5d0ed99d0029c88f1b8fe43cbcdb2abb9af043
KaungZayY <kaungzayyan19@gmail.com>
2024-12-03 10:07:14 +0700

    HomeView Filter

--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -2,14 +2,18 @@
 import Footer from '@/components/Footer.vue'
 import Wrapper from '@/components/Wrapper.vue';
 import AuthNavBar from '@/components/AuthNavBar.vue';
-import SearchBar from '@/components/SearchBar.vue';
 import ItemCard from '@/components/ItemCard.vue';
+import FilterWrapper from '@/components/FilterWrapper.vue';
 
 import axios from 'axios';
-import { reactive, onMounted } from 'vue';
+import { reactive, onMounted, computed } from 'vue';
 import router from '@/router';
 import { useToast } from 'vue-toastification';
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+import FilterByTitle from '@/components/FilterByTitle.vue';
+import FilterByCategory from '@/components/FilterByCategory.vue';
+import FilterByDate from '@/components/FilterByDate.vue';
+import ClearFilter from '@/components/ClearFilter.vue';
 
 const toast = useToast();
 
@@ -17,6 +21,64 @@ const state = reactive({
     items: [],
     isLoading: true
 });
+
+const searchFilter = reactive({
+    title: '',
+});
+
+const dateFilter = reactive({
+    from_date: '',
+    to_date: '',
+});
+
+const dropListFilter = reactive({
+    category: '',
+})
+
+const filteredItems = computed(() => {
+    let items = state.items;
+    if(searchFilter.title !== ''){
+        items = items.filter(item => item.title.includes(searchFilter.title));
+    }
+
+    if(dateFilter.from_date !== '' && dateFilter.to_date !== ''){
+        const fromDate = new Date(dateFilter.from_date);
+        const toDate = new Date(dateFilter.to_date);
+        items = items.filter(item => {
+            const itemDate = new Date(item.created_date);
+            return itemDate >= fromDate && itemDate <= toDate;
+        });
+    }
+
+    if(dropListFilter.category !== ''){
+        items = items.filter(item => item.category_id == dropListFilter.category);
+    }
+
+    return items;
+})
+
+const handleSearch = (search) => {
+    searchFilter.title = search;
+}
+
+const handleFromDate = (fromDate) => {
+    dateFilter.from_date = fromDate;
+};
+
+const handleToDate = (toDate) => {
+    dateFilter.to_date = toDate;
+};
+
+const handleCategory = (category) => {
+    dropListFilter.category = category;
+}
+
+const remoteAllFilterValues = () => {
+    searchFilter.title = '';
+    dateFilter.from_date = '';
+    dateFilter.to_date = '';
+    dropListFilter.category = '';
+};
 
 onMounted(async () => {
     try {
@@ -40,7 +102,12 @@ onMounted(async () => {
 <template>
     <Wrapper>
         <AuthNavBar />
-        <SearchBar />
+        <FilterWrapper>
+            <FilterByCategory @category_filter="handleCategory" />
+            <FilterByTitle @search="handleSearch" />
+            <FilterByDate @from_date="handleFromDate" @to_date="handleToDate"/>
+            <ClearFilter @show_all="remoteAllFilterValues"/>
+        </FilterWrapper>
         <h1 class="text-black dark:text-white text-2xl text-center pt-2">Items</h1>
         <div class="flex flex-row justify-end">
             <a href="#"
@@ -52,7 +119,7 @@ onMounted(async () => {
             <PulseLoader />
         </div>
         <div v-else class="flex flex-col md:flex-row flex-wrap pt-2 pb-8">
-            <ItemCard v-for="item in state.items" :key="item.id" :item="item" />
+            <ItemCard v-for="item in filteredItems" :key="item.id" :item="item" />
         </div>
     </Wrapper>
     <Footer />
